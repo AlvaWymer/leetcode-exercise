e34e076c083d05aa62cab97ac4b90adda373b01a
Yang Naihua <naihua.yang@gmail.com>
2021-11-18 17:06:46 +0100

    init sum

---
+++ easy/src/main/java/Sum.java
@@ -0,0 +1,11 @@
+/**
+ * @className: Sum
+ * @description:
+ * @author: Yang Naihua
+ * @create: 2021-11-18 17:05
+ **/
+public class Sum {
+    public static void main(String[] args) {
+        System.err.println("Hello World");
+    }
+}
